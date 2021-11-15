0ce23f4002b25b46ee3f522f813b2c68e3d95faa
0724654276 <you@example.com>
2021-11-15 14:25:09 +0300

    commit to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #export SECRET_KEY='63hd7733'
-export MAIL_USERNAME = 'george.macharia@student.moringaschool.com'
-export MAIL_PASSWORD = 'kamakia91'
+export MAIL_USERNAME='george.macharia@student.moringaschool.com'
+export MAIL_PASSWORD='kamakia91'
 
 python3.8 manage.py server
