f1fe1112545734210880ee7c403d5748f88901d3
0724654276 <you@example.com>
2021-11-15 09:53:28 +0300

    changes in config

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,16 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import InputRequired,Email,EqualTo
 
 class UpdateProfileForm(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
     submit = SubmitField('Update')
 
 class UploadBlogForm(FlaskForm):
-    title = TextAreaField('Blog Title',validators=[Required()])
-    blog =  TextAreaField('your blog',validators=[Required()])
+    title = TextAreaField('Blog Title',validators=[InputRequired()])
+    blog =  TextAreaField('your blog',validators=[InputRequired()])
     submit = SubmitField('Post')
 
 class CommentsForm(FlaskForm):
-    comment = TextAreaField('Type comment:',validators=[Required()])
+    comment = TextAreaField('Type comment:',validators=[InputRequired()])
     submit = SubmitField('Comment')
