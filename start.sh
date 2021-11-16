9a5bfa587d13e3262998fbc550addae5518d9ab4
0724654276 <you@example.com>
2021-11-16 15:14:05 +0300

    initial

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-#export SECRET_KEY='63hd7733'
+export SECRET_KEY='63hd7733'
 export MAIL_USERNAME='george.macharia@student.moringaschool.com'
 export MAIL_PASSWORD='kamakia91'
+export API_KEY="http://quotes.stormconsultancy.co.uk/random.json (Links to an external site.)"
 
 python3.8 manage.py server
